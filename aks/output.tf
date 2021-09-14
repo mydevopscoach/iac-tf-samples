819babe7e1d99fe2ad9789bf2e26a7ea4843e990
mydevopscoach <30705206+mydevopscoach@users.noreply.github.com>
2021-09-14 08:32:38 -0500

    added code for creating kubeconfig

--- aks/output.tf
+++ aks/output.tf
@@ -17,3 +17,9 @@ output "acr_id" {
 output "acr_login_server" {
   value = azurerm_container_registry.acr.login_server
 }
+
+resource "local_file" "kubeconfig" {
+  depends_on   = [azurerm_kubernetes_cluster.aks]
+  filename     = "kubeconfig"
+  content      = azurerm_kubernetes_cluster.aks.kube_config_raw
+}
